0ed35ceab14a1615010c0a2d9e9e0a14103a76a2
Raccoon <nasty1223@mail.ru>
2018-06-02 03:17:02 +0700

    Add function int stroke_comp

--- 100.c
+++ 100.c
@@ -2,20 +2,20 @@
 #include <time.h>
 #include <stdlib.h>
 int main ()
-int proverka( int Count, int Num)
+int proverka ( int Count, int Num)
 {
-if (Num < 0 || Num > 10 || Num > Count)
-{
-printf("Неверно! Повторите ввод.\n");
-scanf("%d", &Num);
-Num = proverka(Count, Num);
-}
-return Num;
+    if (Num < 0 || Num > 10 || Num > Count)
+    {
+        printf("Неверно! Повторите ввод.\n");
+        scanf("%d", &Num);
+        Num = proverka (Count, Num);
+    }
+    return Num;
 }
 int games()
 {
-int Count = 100, Player=1;
-while (Count>0)
+    int Count = 100, Player = 1;
+    while (Count > 0)
 {
 
 }
@@ -24,15 +24,25 @@ while (Count>0)
 {
 int stroke_user(int Count)
 {
-int Num;
-                      printf("Ваш ход. На столе %d спичек.\n", Count);
-				printf("Сколько спичек Вы берете?\n");
-		scanf("%d", &Num);
-Num=proverka(Count, Num);
-
-Count-=Num;
-return Count;
+    int Num;
+    printf("Ваш ход. На столе %d спичек.\n", Count);
+    printf("Сколько спичек Вы берете?\n");
+    scanf("%d", &Num);
+    Num = proverka (Count, Num);
+    Count -= Num;
+    return Count;
 }
+int stroke_comp(int Count)
+{
+    int Num;
+        Num = rand() % 10 + 1;
+        if (Num > Count || Count < 10)
+        Num = Count;
+        printf("Мой ход. Я взял %d спичек\n", Num);
+        Count -= Num;
+    return Count;
+}
+
 
 }
 return 0;
