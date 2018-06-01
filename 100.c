8d9495566edc50140f84ed398a6d92c25a1729d7
Raccoon <nasty1223@mail.ru>
2018-06-02 01:37:45 +0700

    Add function int 'stroke_user'

--- 100.c
+++ 100.c
@@ -3,6 +3,17 @@
 #include <stdlib.h>
 int main ()
 {
+int stroke_user(int Count)
+{
+int Num;
+                      printf("Ваш ход. На столе %d спичек.\n", Count);
+				printf("Сколько спичек Вы берете?\n");
+		scanf("%d", &Num);
+Num=//будет проверка правильности введенного числа
+
+Count-=Num;
+return Count;
+}
 
 }
 return 0;
