540e009ec6e270fd9403b93512a0ae0bbce0c74d
notlive1999@mail.ru <notlive1999@mail.ru>
2018-06-02 02:36:33 +0700

    function Moneta

--- Moneta.c
+++ Moneta.c
@@ -7,3 +7,13 @@ void Kolichestvo()
     printf("Введите количество монет: ");
     scanf ("%d", &kol);
 }
+void Moneti(int n)
+{
+    int *moneta = new int[kol];
+    int *b = new int[kol];
+    for (int i = 0; i < kol; i++)
+    {
+        moneta[i] = rand() % (100 - 100 + 1) + 100 ;
+        b[i] = 0;
+    }
+}
