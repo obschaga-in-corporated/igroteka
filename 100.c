1ef6bc1e2d0116f2108e38e0847803bc5a2cbf95
Raccoon <nasty1223@mail.ru>
2018-06-02 03:02:52 +0700

    Add function int proverka

--- 100.c
+++ 100.c
@@ -2,23 +2,33 @@
 #include <time.h>
 #include <stdlib.h>
 int main ()
-
-
+int proverka( int Count, int Num)
+{
+if (Num < 0 || Num > 10 || Num > Count)
+{
+printf("Неверно! Повторите ввод.\n");
+scanf("%d", &Num);
+Num = proverka(Count, Num);
+}
+return Num;
+}
 int games()
 {
-int Count = 100, Player=1
+int Count = 100, Player=1;
 while (Count>0)
 {
+
 }
 }
 
+{
 int stroke_user(int Count)
 {
-    int Num;
-       printf("Ваш ход. На столе %d спичек.\n", Count);
-       printf("Сколько спичек Вы берете?\n");
-       scanf("%d", &Num);
-        Num=proverka(Count, Num);
+int Num;
+                      printf("Ваш ход. На столе %d спичек.\n", Count);
+				printf("Сколько спичек Вы берете?\n");
+		scanf("%d", &Num);
+Num=proverka(Count, Num);
 
 Count-=Num;
 return Count;
