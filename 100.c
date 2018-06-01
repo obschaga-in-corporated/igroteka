b82658ed890e06dfeeaf837fd0b7b2f483051979
Raccoon <nasty1223@mail.ru>
2018-06-02 02:35:38 +0700

    Add function int games

--- 100.c
+++ 100.c
@@ -2,14 +2,23 @@
 #include <time.h>
 #include <stdlib.h>
 int main ()
+
+
+int games()
 {
+int Count = 100, Player=1
+while (Count>0)
+{
+}
+}
+
 int stroke_user(int Count)
 {
-int Num;
-                      printf("Ваш ход. На столе %d спичек.\n", Count);
-				printf("Сколько спичек Вы берете?\n");
-		scanf("%d", &Num);
-Num=//будет проверка правильности введенного числа
+    int Num;
+       printf("Ваш ход. На столе %d спичек.\n", Count);
+       printf("Сколько спичек Вы берете?\n");
+       scanf("%d", &Num);
+        Num=proverka(Count, Num);
 
 Count-=Num;
 return Count;
