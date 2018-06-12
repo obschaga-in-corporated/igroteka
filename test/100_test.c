3b138922ac110bc7fcc42e28c95c6c056def0c65
Raccoon <nasty1223@mail.ru>
2018-06-13 04:29:46 +0700

    Test progress_user

--- test/100_test.c
+++ test/100_test.c
@@ -7,3 +7,9 @@ CTEST(quit_suite, quit)
     int rezult = quit();
     ASSERT_INTERVAL(-1, 1, rezult);
 }
+CTEST(progress_user_suite, progress)
+{
+    int answer = 100;
+    int rezult = progress_user(answer);
+    ASSERT_INTERVAL(90, 99, rezult);
+}
