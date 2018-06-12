a20d1606002c53a96f6cb94c4c96294bff0fdcfc
notlive1999@mail.ru <notlive1999@mail.ru>
2018-06-13 01:07:40 +0700

    Moneta.h

---
+++ src/Moneta.h
@@ -0,0 +1,25 @@
+#ifndef __Moneta__
+#define __Moneta__
+
+int output ();
+
+int Kolichestvo();
+
+int *Moneta (int kol, int moneta[]);
+
+int *mass (int kol, int b[]);
+
+int youhod (int kol, int *b);
+
+int yousum (int num, int *moneta, int sumyou);
+
+int comphod (int kol, int *b);
+
+int compsum (int num, int *moneta, int sumcomp);
+
+void comparison (int sumyou, int sumcomp);
+
+int association();
+
+
+#endif
