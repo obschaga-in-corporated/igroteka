395ec77703a9cf9c1c4eb9d917fde91a1b31fb43
Ubunt <ubunt@localhost.localdomain>
2018-06-13 02:40:46 +0700

    Add CodeStyle in ygad.c

--- src/ygad.c
+++ src/ygad.c
@@ -6,34 +6,38 @@
 int number ()
 {
     int num;
+    
     srand(time(NULL));
-    num =1 + rand() %100;
+    num = 1 + rand() %100;
     return num;
 }
 
 int power (int n) 
 {
-    if (n == 0) 
+    if (n == 0) {
         return 1;
+    }
     return 2 * power(n - 1);
 }
 
 int output()
 {
-int raz;
-printf("Если хотите сыграть заново - введите -1, продолжить - 1, выйти - 0: \n");
-scanf("%d", &raz);
-if (raz!= 1 && raz!=0 && raz != -1)
-raz=output();
-return raz;
+    int raz;
+    
+    printf ("Если хотите сыграть заново введите -1, 
+            продолжить - 1, выйти - 0: \n");
+    scanf("%d", &raz);
+    if (raz != 1 && raz != 0 && raz != -1) {
+        raz = output();
+    }
+    return raz;
 }
 
 int proverka (int answer)
 {  
-    if (answer < 0 || answer > 100)
-    {
+    if (answer < 0 || answer > 100) {
         printf ("FATAL ERROR! Пожалуйста, введите новое значение: \n");
-        scanf("%d", &answer);
+        scanf ("%d", &answer);
         answer = proverka (answer);
     }
     return answer;
@@ -41,48 +45,45 @@ int proverka (int answer)
 
 void stroke_user (int answer_user, int num)
 {
-    if (answer_user > num)
-        printf("У вас перелет \n");
-    else
-        printf("У вас недолет \n");
+    if (answer_user > num) {
+        printf ("У вас перелет \n");
+    } else {
+        printf ("У вас недолет \n");
+    }
 }
 int user_hod ()
 {
-    int answer_user;
+    int answer_user, game;
+    
     printf ("Введите число: \n");
-    scanf("%d", &answer_user);
+    scanf ("%d", &answer_user);
     answer_user = proverka(answer_user);
-    if (answer_user==0)
-{
-int game = output ();
-if (game == 0)
-return 0;
-else if (game ==1)
-answer_user = user_hod();
-else 
-return -1;
-}  
+    if (answer_user == 0) {
+        game = output ();
+        if (game == 0) {
+            return 0;
+        } else if (game == 1) {
+            answer_user = user_hod();
+        } else { 
+            return -1;
+        }
+    }  
     return answer_user;
 }
 
 int stroke_comp (int answer_comp, int num, int try)
 { 
     int stupen = power (try), i;
-    if (try == 5)
-    {
+    
+    if (try == 5) {
         i = 1 + rand() % 2;
         answer_comp += i;	    
-    }   
-    else
-    {
-        if (answer_comp > num)
-        {
-            printf("У компьютера перелет \n");
+    } else {
+        if (answer_comp > num) {
+            printf ("У компьютера перелет \n");
 	    answer_comp -= (50 / stupen);
-	}
-	else
-	{
-	    printf("У компьтера недолет \n");
+        } else {
+            printf ("У компьтера недолет \n");
             answer_comp += (50 / stupen);
         }
     }
@@ -91,44 +92,53 @@ int stroke_comp (int answer_comp, int num, int try)
 
 void rezult (int answer_user, int answer_comp, int num)
 {
-    if (answer_user == num && answer_comp != num)
-        printf("Поздравляем! Вы победили! \n"); 
-    if (answer_user != num && answer_comp == num)
-        printf("Вы проиграли! Попробуйте еще раз...\n Правильный ответ: %d\n", num);
-    if (answer_user == num && answer_comp == num)
+    if (answer_user == num && answer_comp != num) {
+        printf ("Поздравляем! Вы победили! \n"); 
+    }
+    if (answer_user != num && answer_comp == num) {
+        printf ("Вы проиграли! Попробуйте еще раз...\n 
+                Правильный ответ: %d\n", num);
+    }
+    if (answer_user == num && answer_comp == num) {
         printf("Ничья!\n");
-    if (answer_user != num && answer_comp != num)
-        printf("Никто не выиграл.\n Правильный ответ: %d\n", num); 
+    }
+    if (answer_user != num && answer_comp != num) {
+        printf("Никто не выиграл.\n Правильный ответ: %d\n", num);
+    }	
 }       
 
 int condition ()
 {
     int answer_user, num = number (), game, answer_comp= 50, tryCount = 0;    
-   answer_user=user_hod();
-  if (answer_user==0)
+    
+    answer_user = user_hod();
+    if (answer_user == 0) {
         return 0;
-        else if (answer_user == -1)
-            return 1;
-  
+    } else if (answer_user == -1) {
+        return 1;
+    }
 
-    while (answer_user != num && answer_comp != num && tryCount < 5)
-    {
+    while (answer_user != num && answer_comp != num && tryCount < 5) {
         tryCount ++;
         printf ("Количество оставшихся попыток = %d\n", 6-tryCount);
         answer_comp = stroke_comp (answer_comp, num, tryCount);
-        stroke_user (answer_user, num);
+        
+	stroke_user (answer_user, num);
         answer_user = user_hod();
-     if (answer_user==0)
-        return 0;
-        else if (answer_user == -1)
+        if (answer_user == 0) {
+            return 0;
+        } else if (answer_user == -1) {
             return 1;
+        }
     }
+    
     rezult (answer_user, answer_comp, num);
     game = output ();
-if (game == 1 || game==-1)
-return 1;
-else
-return 0;
+    if (game == 1 || game == -1) {
+        return 1;
+    } else {
+        return 0;
+    }
 }
 
 
