62498ff0a806c03dc2522d09f87c19020c505e01
notlive1999@mail.ru <notlive1999@mail.ru>
2018-06-13 02:46:25 +0700

    Summ monet you

--- test/Moneta_test.c
+++ test/Moneta_test.c
@@ -5,19 +5,19 @@
 
 CTEST (Kolich, Correct) {
     int kol = Kolichestvo();
-    ASSERT_INTERVAL (1,100, kol);
+    ASSERT_INTERVAL (1, 100, kol);
 }
 CTEST (Moneta, Correct) {
     int kol = 5;
-    int *moneta=NULL;
+    int *moneta = NULL;
     moneta = Moneta(kol, moneta);
     ASSERT_INTERVAL (-100, 100, *moneta);
 }
 CTEST (Mass, Correct) {
     int kol = 5;
-    int *b=NULL;
-    b = mass(kol, b);
-    for(int i=0; i<kol;i++) {
+    int *b = NULL;
+    b = mass (kol, b);
+    for (int i = 0; i < kol; i++) {
         ASSERT_EQUAL (0, b[i]);
     }
 }
@@ -32,4 +32,12 @@ CTEST (Hodyou1, Correct) {
     int b[5] = {0, 0, 0, 0, 0};
     int num = youhod (kol, b);
     ASSERT_INTERVAL (-1, 5, num);
+}
+CTEST (Sumyou, Correct) {
+    int num = 1;
+    int *moneta = NULL;
+    moneta = Moneta (2, moneta);
+    int sumyou = 0;
+    sumyou = yousum (num, moneta, sumyou);
+    ASSERT_INTERVAL (-100, 100, sumyou);
 }
