7902a122eaba83d3fdf9273df74203695dd7db1e
notlive1999@mail.ru <notlive1999@mail.ru>
2018-06-13 01:20:11 +0700

    Test moneta

--- test/Moneta_test.c
+++ test/Moneta_test.c
@@ -7,3 +7,9 @@ CTEST (Kolich, Correct) {
     int kol = Kolichestvo();
     ASSERT_INTERVAL (1,100, kol);
 }
+CTEST (Moneta, Correct) {
+    int kol = 5;
+    int *moneta=NULL;
+    moneta = Moneta(kol, moneta);
+    ASSERT_INTERVAL (-100, 100, *moneta);
+}
