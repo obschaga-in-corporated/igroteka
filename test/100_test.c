0e13c80c953a61b92cf756c2a455a0c106f4c054
Ubunt <ubunt@localhost.localdomain>
2018-06-14 09:09:38 +0700

    Delete few tests

--- test/100_test.c
+++ test/100_test.c
@@ -2,23 +2,7 @@
 #include "ctest.h"
 #include <stdlib.h>
 #include <stdio.h>
-CTEST(quit_suite, quit)
-{
-    int rezult = quit();
-    ASSERT_INTERVAL(-1, 1, rezult);
-}
-CTEST(progress_user_suite, progress)
-{
-    int answer = 100;
-    int rezult = progress_user(answer);
-    ASSERT_INTERVAL(90, 99, rezult);
-}
-CTEST(progress_user_prog_suite, progress)
-{
-    int answer = 56;
-    int rezult = progress_user(answer);
-    ASSERT_INTERVAL(46,55, rezult);
-}
+
 CTEST (Trust, Correct)
 {
     int Count = 11;
@@ -26,6 +10,7 @@ CTEST (Trust, Correct)
     Num = trust (Count, Num);
     ASSERT_INTERVAL (1, 10, Num);
 }
+
 CTEST (Comp, Correct)
 {
     int Count = 100;
