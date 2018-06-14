6bce6cffc6f1c8e628bb0c0e8c64f8454a7ca1eb
Ubunt <ubunt@localhost.localdomain>
2018-06-14 09:10:52 +0700

    Delete few tests

--- test/Moneta_test.c
+++ test/Moneta_test.c
@@ -3,10 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-CTEST (Kolich, Correct) {
-    int kol = Kolichestvo();
-    ASSERT_INTERVAL (1, 100, kol);
-}
+
 CTEST (Moneta, Correct) {
     int kol = 5;
     int *moneta = NULL;
@@ -21,18 +18,7 @@ CTEST (Mass, Correct) {
         ASSERT_EQUAL (0, b[i]);
     }
 }
-CTEST (Hodyou, Correct) {
-    int kol = 5;
-    int b[5] = {1, 1, 0, 1, 1};
-    int num = youhod (kol, b);
-    ASSERT_EQUAL (3, num);
-}
-CTEST (Hodyou1, Correct) {
-    int kol = 5;
-    int b[5] = {0, 0, 0, 0, 0};
-    int num = youhod (kol, b);
-    ASSERT_INTERVAL (-1, 5, num);
-}
+
 CTEST (Sumyou, Correct) {
     int num = 1;
     int *moneta = NULL;
